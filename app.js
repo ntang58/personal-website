8a21e500a60c734c7eb3fabe188d6b539912a2e1
Nicholas <ntang58@yahoo.com>
2018-04-18 19:56:25 +0000

    added routes in server to landing and about

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+// --- app require ---
+var express = require("express"),
+    app = express();
+
+// --- app config ---    
+app.set("view engine", "ejs");
+
+// -- app routes ---
+
+app.get("/",function(req,res){
+    res.render("landing");
+});
+app.get("/about", function(req,res){
+   res.render("about"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server is running");
+});
+
+
